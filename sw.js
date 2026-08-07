71be4e50f186b68b1fdb5818ae5118ee72d9984d
Ewrc76 <ericramos6@gmail.com>
2026-08-06 21:39:19 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,47 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('podocheck-v1').then((cache) => {
-      return cache.addAll(['./', './index.html', './manifest.json', './Podo.png']);
+const CACHE_NAME = 'podocheck-cache-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './Podo.png'
+];
+
+// Instalación e independización inmediata del caché nuevo
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+// Eliminación de cachés antiguos (v1.0)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Estrategia: Red primero (para obtener siempre la versión v2.0 actualizada), si no hay red usa caché
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (response && response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
